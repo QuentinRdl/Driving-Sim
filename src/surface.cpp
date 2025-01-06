606d486b89a3bf78466b9b12a93f9c3731841f7b
R-Gld <romain.galland@rgld.r>
2025-01-06 16:14:23 +0100

    [TMP] Temporary testing on surface.cpp.

--- src/surface.cpp
+++ src/surface.cpp
@@ -19,3 +19,32 @@ namespace Driving_Sim {
     }
 
 } // std
+
+
+/**
+ *
+ *Temp Testing.
+ *
+ * @return 0
+ */
+int main() {
+
+    const Driving_Sim::surface asphalte("Asphalte", 0.85);
+    const Driving_Sim::surface sable("Sable", 0.6);
+    const Driving_Sim::surface verglas("Verglas", 0.1);
+
+
+    for (const Driving_Sim::surface &surface_ : {asphalte, sable, verglas}) {
+        surface_.display();
+
+        constexpr double mass = 1500.0;
+        constexpr double g = 9.81;
+        const double traction_max = surface_.getFrictionCoeff() * mass * g;
+
+        // P_g = m*g (N)
+        std::cout << "Force de traction max sur " << surface_.getName() << ": " << traction_max << "N" << std::endl << std::endl << std::endl;
+    }
+
+
+    return 0;
+}
