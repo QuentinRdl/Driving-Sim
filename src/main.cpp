60a6267f55586959fecd8f1756b93cc188eab55f
R-Gld <romain.galland@rgld.r>
2025-01-08 18:10:57 +0100

    [Fix] t2 increased

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,13 @@ int main() {
     Driving_Sim::Car car1(4.5, 2.5);
     car1.setInputs(1.5, 0);
     car1.setInitialConditions(50, 20, 20, 0);
-    double t1 = 0.1;
+    double t1 = 5;
     std::string file1 = "car1.txt";
 
     Driving_Sim::Car car2(2.5, 3.5);
     car2.setInputs(-1, 0.05);
     car2.setInitialConditions(0, 0, 40, 0.5);
-    double t2 = 0.15;
+    double t2 = 150;
     std::string file2 = "car2.txt";
 
     std::thread th1(&Driving_Sim::Car::simulateTrajectory, std::ref(car1), std::ref(t1));
