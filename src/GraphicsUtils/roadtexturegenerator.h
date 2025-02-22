29b01407943f3bfe40fbf768125763af0e100d8d
R-Gld <romain.galland@rgld.r>
2025-02-22 02:02:24 +0100

    [Refactor] Update getExtremities and generate_road_texture functions to support texture size and mirroring

--- src/GraphicsUtils/roadtexturegenerator.h
+++ src/GraphicsUtils/roadtexturegenerator.h
@@ -1,6 +1,8 @@
 #ifndef ROADTEXTUREGENERATOR_H
 #define ROADTEXTUREGENERATOR_H
 
+#include <complex>
+
 #include "game.h"
 #include "segmenttype.h"
 #include "texturemanager.h"
@@ -12,7 +14,15 @@ struct RoadTexture {
     sf::Vector2f point2;
 };
 
-inline const sf::Vector2f *getExtremities(const SegmentType::Value type, const bool mirrorX = false, const bool mirrorY = false) {
+/**
+ *
+ * @param type the type of the segment
+ * @param size the size of the texture
+ * @param mirror_x true if the texture should be mirrored on the x-axis, false otherwise
+ * @param mirror_y true if the texture should be mirrored on the y-axis, false otherwise
+ * @return an array of two `sf::Vector2f`, the graphic extremities of the segment
+ */
+inline const sf::Vector2f *getExtremities(const SegmentType::Value type, const sf::Vector2u& size, const bool mirror_x = false, const bool mirror_y = false) {
     static sf::Vector2f extremities[2];
     switch (type) {
         case SegmentType::SMALL_STRAIGHT:
@@ -45,17 +55,46 @@ inline const sf::Vector2f *getExtremities(const SegmentType::Value type, const b
             break;
         default: throw std::invalid_argument("Invalid segmentType: not implemented yet maybe ?");
     }
+
+    // Appliquer la transformation de miroir sur l'axe X
+    if (mirror_x) {
+        extremities[0].x = static_cast<float>(size.x) - extremities[0].x;
+        extremities[1].x = static_cast<float>(size.x) - extremities[1].x;
+    }
+    // Appliquer la transformation de miroir sur l'axe Y
+    if (mirror_y) {
+        extremities[0].y = static_cast<float>(size.y) - extremities[0].y;
+        extremities[1].y = static_cast<float>(size.y) - extremities[1].y;
+    }
+
     return extremities;
 }
 
-inline RoadTexture generate_road_texture(const TextureManager &texture_manager, const SegmentType::Value type, const bool mirror_x = false, const bool mirror_y = false) {
-    const sf::Vector2f *extremities = getExtremities(type, mirror_x, mirror_y);
+inline RoadTexture generate_road_texture(const TextureManager &texture_manager, const SegmentType::Value segment_type,
+                                         const bool mirror_x = false, const bool mirror_y = false) {
+    const sf::Texture& baseTexture = texture_manager.getTexture(segment_type);
+
+    const sf::Vector2f *extremities = getExtremities(segment_type, baseTexture.getSize(), mirror_x, mirror_y);
 
     RoadTexture road_texture;
     road_texture.point1 = extremities[0];
     road_texture.point2 = extremities[1];
-    road_texture.texture = &texture_manager.getTexture(type);
+    road_texture.texture = &baseTexture;
     road_texture.sprite = sf::Sprite(*road_texture.texture);
+
+    const int base_x = static_cast<int>(baseTexture.getSize().x);
+    const int base_y = static_cast<int>(baseTexture.getSize().y);
+
+    sf::IntRect rect(0, 0, base_x, base_y);
+    if (mirror_x) {
+        rect.left = base_x;
+        rect.width = -base_x;
+    }
+    if (mirror_y) {
+        rect.top = base_y;
+        rect.height = -base_y;
+    }
+    road_texture.sprite.setTextureRect(rect);
 
     return road_texture;
 }
