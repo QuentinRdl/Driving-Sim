ce0f4dab7fd0765d8e829b3da9f78c38a2a34f5a
R-Gld <romain.galland@rgld.r>
2025-02-24 20:10:03 +0100

    [Fix] Create a header to include some classes to avoid circular dependencies problems.

---
+++ src/Headers/main_classes.h
@@ -0,0 +1,15 @@
+//
+// Created by rgld_ on 24/02/25.
+//
+
+#ifndef CLASSES_H
+#define CLASSES_H
+
+/**
+ * This File is used to avoid circular dependencies problems.
+ */
+
+class Game;
+class Circuit;
+
+#endif //CLASSES_H
