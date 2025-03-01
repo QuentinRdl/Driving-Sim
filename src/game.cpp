11c4aab6452354b6640553047d4ee8e58e31650c
R-Gld <romain.galland@rgld.r>
2025-03-01 01:14:14 +0100

    [Refactor] Adjust zoom factor initialization and update circuit segment types for improved gameplay experience

--- src/game.cpp
+++ src/game.cpp
@@ -14,17 +14,15 @@ float Game::getZoomFactor() const {
 /**
  * Constructor
  */
-Game::Game(): zoom_factor(0.8) {
+Game::Game(): zoom_factor(0.4) {
     window = std::make_unique<sf::RenderWindow>(sf::VideoMode::getDesktopMode(), "Interface Graphique", sf::Style::Default);
     window->setVerticalSyncEnabled(true);
     texture_manager = {};
 
     // Data given by QRadlo in his tests (cf Physics4Real#src/TheLastOneISwear.cpp).
-    Vehicle vehicle(1700.0, 1.5, 1.5, 0.5, 150000.0, 40000.0, 580, 450, 0);
-    car = std::make_unique<Car>(this, vehicle);
+    car = std::make_unique<Car>(this);
 
     circuit = std::make_unique<Circuit>(this);
-
     updateCircuit();
 }
 
@@ -47,13 +45,13 @@ void Game::manageEvents() {
                 if (event.key.code == sf::Keyboard::F)            fps_counter.toggle();
                 else if (event.key.code == sf::Keyboard::Escape)  window->close();
                 else if (event.key.code == sf::Keyboard::F7)      printf("Car position: %f / %f\n Lacet: %f\n\n",
-                                                                        car->vehicle.x, car->vehicle.y, car->vehicle.r);
+                                                                        car->vehicle->x, car->vehicle->y, car->vehicle->r);
             break;
             case sf::Event::MouseWheelScrolled:
                 if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
                     circuit->needUpdate();
-                    if (event.mouseWheelScroll.delta > 0)    zoom_factor += 0.01;
-                    else                                     zoom_factor = (zoom_factor > 0 ? zoom_factor - 0.01 : 0);
+                    if (event.mouseWheelScroll.delta > 0)    zoom_factor += 0.01f;
+                    else                                     zoom_factor -= (zoom_factor > 0 ? 0.01f : zoom_factor);
                 }
                 break;
             case sf::Event::Closed:
@@ -62,7 +60,6 @@ void Game::manageEvents() {
             default: break;
         }
     }
-
 }
 
 
@@ -81,11 +78,10 @@ void Game::update() {
 
 void Game::updateCircuit() const {
     circuit->resetSegments();
-    circuit->setOrigin(ResourceType::Value::SEGMENT_LARGE_TURN, { 500, 500 },
-        0.f, true, true);
-    circuit->join(ResourceType::Value::SEGMENT_LARGE_TURN, 270);
-    circuit->join(ResourceType::Value::SEGMENT_LARGE_TURN, 0);
-    circuit->join(ResourceType::Value::SEGMENT_LARGE_TURN, 90);
+    circuit->setOrigin(ResourceType::Value::SEGMENT_SMALL_STRAIGHT, { 500, 40 });
+    circuit->join(ResourceType::Value::SEGMENT_S_TURN);
+    circuit->join(ResourceType::Value::SEGMENT_MEDIUM_TURN);
+    // std::cout << "" << std::endl;
 }
 
 void Game::render() const {
