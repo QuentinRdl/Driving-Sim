9c0f12798161172876c7702e4740f080496e959d
QuentinRdl <quentinradlo.pro@gmail.com>
2025-03-10 16:04:02 +0100

    [REFINE] - Commented out main function code for future implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include "game.h"
 #include "vehicle.h"
 int main() {
+    /*
     Vehicle v;
     v.plotTest();
     v.plotTestIterative();
+    */
 
-    /*
     Game game;
 
     while(game.running()) {
         game.update();
         game.render();
     }
-    */
 
 
     return EXIT_SUCCESS;
