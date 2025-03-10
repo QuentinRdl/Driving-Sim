a6e620d7def248e7dbed8f29dbeac3837906c6ca
R-Gld <romain.galland@rgld.r>
2025-03-10 17:36:48 +0100

    [FIX] - Fix some backgrounds going in front of texts.

--- src/debugmode.cpp
+++ src/debugmode.cpp
@@ -22,29 +22,37 @@ void DebugMode::renderOn(sf::RenderWindow &window) const {
     if (!enabled) return;
 
     const sf::Vector2u windowSize = window.getSize();
+    constexpr float lineSpaceing = 25.f;
+    constexpr float topMargin = 20.f;
+    constexpr float margin = 20.f;
 
-    for (size_t i = 0; i < texts.size(); ++i) {
-        sf::Text text = texts.at(i);
+    for (int i = 0; i < backgrounds.size(); ++i) {
         sf::RectangleShape bg = backgrounds.at(i);
-
-        constexpr float lineSpaceing = 25.f;
-        constexpr float topMargin = 20.f;
-        constexpr float margin = 20.f;
+        sf::Text text = texts.at(i);
         const float textWidth = text.getLocalBounds().width;
 
         sf::Vector2f pos(static_cast<float>(windowSize.x) - textWidth - margin,
                          topMargin + static_cast<float>(i) * lineSpaceing);
 
-        text.setPosition(pos);
-        bg.setPosition(pos * 1.f);
+        bg.setPosition(pos + sf::Vector2f(-5.0f, 0.0f));
 
         window.draw(bg);
+    }
+    for (size_t i = 0; i < texts.size(); ++i) {
+        sf::Text text = texts.at(i);
+
+        const float textWidth = text.getLocalBounds().width;
+
+        sf::Vector2f pos(static_cast<float>(windowSize.x) - textWidth - margin,
+                         topMargin + static_cast<float>(i) * lineSpaceing);
+        text.setPosition(pos);
         window.draw(text);
     }
 }
 
 void DebugMode::update(const Car *car) {
     texts.clear();
+    backgrounds.clear();
 
     auto round = [](const double value, const int precision = 3) {
         char buffer[32];
@@ -53,22 +61,22 @@ void DebugMode::update(const Car *car) {
     };
     auto &v = car->vehicle;
 
-    const sf::Color gray(120, 120, 120);
+    const sf::Color gray(200, 200, 200);
 
     addLine("Car", 28, sf::Color(255, 84, 71));
-    addLine("Constants: ", 25, gray);
+    addLine("Constants:", 25, gray);
     addLine("Mass: " + round(v->mass, 0), 20);
     addLine("Coeff Friction (Front): " + round(v->mu_front, 2), 20);
     addLine("Coeff Friction (Rear): " + round(v->mu_rear, 2), 20);
     addLine("Moment d'inertie: " + round(v->I, 1), 20);
-    addLine("Constante gravitationnelle: " + round(v->g, 2), 20);
     addLine("Longitudinal rigidity (N): " + round(v->Cx, 0), 20);
     addLine("Lateral rigidity (N/rad): " + round(v->Cy, 0), 20);
     addLine("", 10);
-    addLine("Variables: ", 25, gray);
+    addLine("Variables:", 25, gray);
     addLine("Position (X/Y): " + round(v->x, 1) + " / " + round(v->y, 1), 20);
-    addLine("Speed: " + round(v->vx), 20);
-    addLine("PSI (in deg): " + round(std::fmod(radToDeg(v->psi), 360.0f)), 20); // direction in ° mod 360
+    addLine("Speed (km/h): " + round(v->vx*3.6), 20);
+    addLine("Heading (in deg): " + round(std::fmod(radToDeg(v->psi), 360.0f)), 20); // heading in ° mod 360
+    addLine("Steering Wheel (in deg): " + round(car->getSteeringAngle()), 20);
     addLine("Lacet (in deg/s): " + round(radToDeg(v->lacet)), 20); // Lacet in ° mod 360
 }
 
@@ -80,8 +88,8 @@ void DebugMode::addLine(const std::string &content, const uint size, const sf::C
     text.setString(content);
 
     sf::RectangleShape background;
-    background.setSize(sf::Vector2f(text.getLocalBounds().width*1.1f, text.getLocalBounds().height*1.1f));
-    background.setFillColor(sf::Color(0, 0, 0, 255/2));
+    background.setSize(sf::Vector2f(text.getLocalBounds().width*1.3f, text.getLocalBounds().height + 25.f/4.f));
+    background.setFillColor(sf::Color(64, 64, 64, 255/2));
 
     texts.push_back(text);
     backgrounds.push_back(background);
